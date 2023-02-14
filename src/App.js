a51d4bd0e1e0d40c6cfcd8148da3dd74358ac3fb
LR.btcmp <loretta.rehm@gmail.com>
2023-02-14 21:10:10 +0000

    Created quiz first question and set of answers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
+import Quiz from './components/Quiz';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>FAB 4</h1>
+        <Quiz />
       </header>
     </div>
   );
