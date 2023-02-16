4eebad0ec004aaac182b6db3d69e22ea5cd595a5
LR.btcmp <loretta.rehm@gmail.com>
2023-02-16 21:18:06 +0000

    Material UI Quiz styling

--- src/components/Question/quizLogic.js
+++ src/components/Question/quizLogic.js
@@ -1,96 +1,121 @@
 import React, {useState} from "react";
 import "./Quiz.css";
 import questionList from "./questionList";
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    Box,
+    Badge,
+    Chip,
+    Grid
+} from '@mui/material';
+// import { Box } from "@mui/system";
+
 // import { Link } from 'react-router-dom'
 
 function QuizLogic() {
 
-  let [currentQues, setcurrentQues] = useState(0)
-  let [correctAnswers, setcorrectAnswers] = useState(0)
-  let [trueFalse, settrueFalse] = useState(true)
+    let [currentQues, setcurrentQues] = useState(0)
+    let [correctAnswers, setcorrectAnswers] = useState(0)
+    let [trueFalse, settrueFalse] = useState(true)
 
-  // find the correct anwers
-  const anwers = questionList[currentQues].answerOptions.filter((correct) => correct.rightChoice === true)
-  const handleCorrect = (correct) => {
+    // find the correct anwers
+    const anwers = questionList[currentQues].answerOptions.filter((correct) => correct.rightChoice === true)
+    const handleCorrect = (correct) => { // turn to congratulate page when you finish
+        if (currentQues === questionList.length - 1) {
+            setcurrentQues(questionList.length - 1)
 
-    // turn to congratulate page when you finish
-    if (currentQues === questionList.length-1){
-      setcurrentQues(questionList.length-1)
+            // next question when you know the correct answer
+        } else if (correct === "recorrect") {
+            setcurrentQues(count => count + 1)
+            settrueFalse(true)
 
-    // next question when you know the correct answer
-    } else if ( correct === "recorrect") {
-      setcurrentQues(count => count + 1)
-      settrueFalse(true)
-
-    // next question when you correct
-    } else if (correct) {
-      setcorrectAnswers(count => count + 1)
-      setcurrentQues(count => count + 1)
-      settrueFalse(true)
-    // show the answer for you when you false
-    } else {
-      settrueFalse(false)
+            // next question when you correct
+        } else if (correct) {
+            setcorrectAnswers(count => count + 1)
+            setcurrentQues(count => count + 1)
+            settrueFalse(true)
+            // show the answer for you when you false
+        } else {
+            settrueFalse(false)
+        }
     }
-}
 
-  return(
-    <div className="quiz-body">
+    return (<div className="quiz-body">
 
-       {/* conditional rendering for finish all question */}
-       { currentQues !== questionList.length-1 ?
-
-      // rendering remaining questions if unanswered.
-        <div className="question-container">
-
-            {/* Question Title */}
-            <div className="question-text">
-                <h4>Question {currentQues+1} / {questionList.length}</h4>
-                <h3>{questionList[currentQues].questionText}</h3>
+        {/* conditional rendering for finish all question */}
+        {
+        currentQues !== questionList.length-1 ?
+        
+              // rendering remaining questions if unanswered.
+              <Card elevation={12} sx={{ px: 'calc(2px + 1.5vw)', py: 'calc(2px + 1.5vw)' }} style={{display: 'inline-block' }}>
+        
+                    {/* Question Title */}
+                    <CardContent className="question-text">
+                    <Box sx={{ flexGrow: 1 }}>
+                      <Grid container spacing={2}>
+                        <Grid xs={9}>
+                          <Box sx={{ textAlign: 'left', m: '1vw' }}>Question {currentQues+1} / {questionList.length}</Box>
+                        </Grid>
+                        <Grid xs={3}>
+                        {/* How many correct answer */}
+                          <Badge badgeContent={correctAnswers} color="success" showZero>
+                            <Chip label="Correct" variant="outlined"/>
+                          </Badge>
+                        </Grid>
+                      </Grid>
+                      </Box>
+                        <Box sx={{ textAlign: 'justify', m: '1vw', fontSize: '1.6rem'}}>
+                          {questionList[currentQues].questionText}  
+                        </Box>
+                    </CardContent>
+                    {/* conditional rendering for true or false answers */}
+                    { trueFalse ? 
+                    <div className="options-section">
+        
+                    {/* render each Answer Option when you true */}
+                    {questionList[currentQues].answerOptions.map((answerOptions) => (
+                      
+                    // it will passing the true / false argument when you click the button
+                    // <button className="optionBtn" key={answerOptions.answerText} onClick={()=> handleCorrect(answerOptions.rightChoice) }>
+                    // {answerOptions.answerText}
+                    // </button>
+                    <Button variant="outlined" key={answerOptions.answerText} onClick={()=> handleCorrect(answerOptions.rightChoice)} sx={{ border: 0 }}>
+                      {answerOptions.answerText}
+                    </Button>
+                    ))}
+                    </div>
+                    :
+                    // render the correct answer for you when you false
+                    <div className="displaytheanswer">
+                    <p>The correct answer is {anwers[0].answerText} </p>
+                    <Button variant="outlined" onClick={()=> handleCorrect("recorrect") }>
+                    Ok!
+                    </Button>
+                    </div>
+                    }
+        
+                </Card>
+                :
+                // rendering quiz results upon completion of all questions.
+                <Card elevation={12} sx={{ px: 'calc(2px + 1.5vw)', py: 'calc(2px + 1.5vw)' }} style={{display: 'inline-block' }}>
+                  <CardContent>
+                    <Box sx={{ textAlign: 'center', fontSize: '1.6rem'}}>
+                      Congratulations
+                    </Box>
+                    <span>Well done! You've completed the quiz game.</span>
+                    <br/>
+                    <span>You have got {correctAnswers} correct answers.</span>
+                  </CardContent>
+                  <CardActions>{/* <Link to="/">Try anthoer quiz</Link> */}</CardActions>
+                </Card>
+        }
+        
+        
             </div>
+    );
+    }
 
-            {/* How many correct answer */}
-            <div className="correct-Answers">
-                <h5>Correct : {correctAnswers}</h5>
-            </div>
-
-            {/* conditional rendering for true or false answers */}
-            { trueFalse ? 
-            <div className="options-section">
-
-            {/* render each Answer Option when you true */}
-            {questionList[currentQues].answerOptions.map((answerOptions) => (
-              
-            // it will passing the true / false argument when you click the button
-            <button className="optionBtn" key={answerOptions.answerText} onClick={()=> handleCorrect(answerOptions.rightChoice) }>
-            {answerOptions.answerText}
-            </button>
-            ))}
-            </div>
-            :
-            // render the correct answer for you when you false
-            <div className="displaytheanswer">
-            <h2>The correct answer is {anwers[0].answerText} </h2>
-            <button className="optionBtn" onClick={()=> handleCorrect("recorrect") }>
-            Ok!
-            </button>
-            </div>
-            }
-
-        </div>
-        :
-        // rendering quiz results upon completion of all questions.
-        <div className="quizEnd-container">
-          <h1>Congratulation</h1>
-          <span>Well done! You've completed the quiz game.</span>
-          <br/>
-          <span>Nailed it ! {correctAnswers} Correct.</span>
-          {/* <Link to="/">Try anthoer quiz</Link> */}
-        </div>
-}
-
-
-    </div>
-  );
-}
-
-export default QuizLogic;
+    export default QuizLogic;
