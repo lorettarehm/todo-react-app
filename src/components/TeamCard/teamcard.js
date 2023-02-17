08964869b8a09d07e7c0a71de6ab9fceb2241a8e
Husain Hidayah <husain.fikri@gmail.com>
2023-02-17 16:13:00 +0000

    add glassmorphism but no responsive yet

--- src/components/TeamCard/teamcard.js
+++ src/components/TeamCard/teamcard.js
@@ -78,39 +78,7 @@ const TeamCard = () => {
       </div> */}
       <div className="container">
         <div className="row">
-          <div className="col-md-6">
-            <div className="card p-0">
-              <div className="card-image">
-                <img
-                  src="https://images.pexels.com/photos/2746187/pexels-photo-2746187.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                  alt=""
-                />
-              </div>
-              <div className="card-content d-flex flex-column align-items-center">
-                <h4 className="pt-2">SomeOne Famous</h4>
-                <h5>Creative Desinger</h5>
-
-                <ul className="social-icons d-flex justify-content-center">
-                  <li style={{ "--i": 1 }}>
-                    <a href="/">
-                      <span className="fab fa-facebook"></span>
-                    </a>
-                  </li>
-                  <li style={{ "--i": 2 }}>
-                    <a href="/">
-                      <span className="fab fa-twitter"></span>
-                    </a>
-                  </li>
-                  <li style={{ "--i": 3 }}>
-                    <a href="/">
-                      <span className="fab fa-instagram"></span>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-6">
+          <div className="col">
             <div className="card p-0">
               <div className="card-image">
                 <img
@@ -119,7 +87,7 @@ const TeamCard = () => {
                 />
               </div>
               <div className="card-content d-flex flex-column align-items-center">
-                <h4 className="pt-2">SomeOne Famous</h4>
+                <h4 className="pt-2">Card 1</h4>
                 <h5>Creative Desinger</h5>
 
                 <ul className="social-icons d-flex justify-content-center">
@@ -142,41 +110,7 @@ const TeamCard = () => {
               </div>
             </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-md-6">
-            <div className="card p-0">
-              <div className="card-image">
-                <img
-                  src="https://images.pexels.com/photos/2746187/pexels-photo-2746187.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                  alt=""
-                />
-              </div>
-              <div className="card-content d-flex flex-column align-items-center">
-                <h4 className="pt-2">SomeOne Famous</h4>
-                <h5>Creative Desinger</h5>
-
-                <ul className="social-icons d-flex justify-content-center">
-                  <li style={{ "--i": 1 }}>
-                    <a href="/">
-                      <span className="fab fa-facebook"></span>
-                    </a>
-                  </li>
-                  <li style={{ "--i": 2 }}>
-                    <a href="/">
-                      <span className="fab fa-twitter"></span>
-                    </a>
-                  </li>
-                  <li style={{ "--i": 3 }}>
-                    <a href="/">
-                      <span className="fab fa-instagram"></span>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-6">
+          <div className="col">
             <div className="card p-0">
               <div className="card-image">
                 <img
@@ -185,7 +119,71 @@ const TeamCard = () => {
                 />
               </div>
               <div className="card-content d-flex flex-column align-items-center">
-                <h4 className="pt-2">SomeOne Famous</h4>
+                <h4 className="pt-2">Card 2</h4>
+                <h5>Creative Desinger</h5>
+
+                <ul className="social-icons d-flex justify-content-center">
+                  <li style={{ "--i": 1 }}>
+                    <a href="/">
+                      <span className="fab fa-facebook"></span>
+                    </a>
+                  </li>
+                  <li style={{ "--i": 2 }}>
+                    <a href="/">
+                      <span className="fab fa-twitter"></span>
+                    </a>
+                  </li>
+                  <li style={{ "--i": 3 }}>
+                    <a href="/">
+                      <span className="fab fa-instagram"></span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card p-0">
+              <div className="card-image">
+                <img
+                  src="https://images.pexels.com/photos/381843/pexels-photo-381843.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                  alt=""
+                />
+              </div>
+              <div className="card-content d-flex flex-column align-items-center">
+                <h4 className="pt-2">Card 3</h4>
+                <h5>Creative Desinger</h5>
+
+                <ul className="social-icons d-flex justify-content-center">
+                  <li style={{ "--i": 1 }}>
+                    <a href="/">
+                      <span className="fab fa-facebook"></span>
+                    </a>
+                  </li>
+                  <li style={{ "--i": 2 }}>
+                    <a href="/">
+                      <span className="fab fa-twitter"></span>
+                    </a>
+                  </li>
+                  <li style={{ "--i": 3 }}>
+                    <a href="/">
+                      <span className="fab fa-instagram"></span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card p-0">
+              <div className="card-image">
+                <img
+                  src="https://images.pexels.com/photos/381843/pexels-photo-381843.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                  alt=""
+                />
+              </div>
+              <div className="card-content d-flex flex-column align-items-center">
+                <h4 className="pt-2">Card 4</h4>
                 <h5>Creative Desinger</h5>
 
                 <ul className="social-icons d-flex justify-content-center">
