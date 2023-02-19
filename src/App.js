5b36a56b282ead3ccddc8efd93554f1bd0714e65
Husain Hidayah <husain.fikri@gmail.com>
2023-02-19 13:31:43 +0000

    enable teamcard

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./components/Header/header.css";
 import QuizLogic from "./components/Question/quizLogic.js";
 import HeaderPart from "./components/Header/header.js";
 import Footer from "./components/Footer/footer.js";
-// import TeamCard from "./components/TeamCard/teamcard.js";
+import TeamCard from "./components/TeamCard/teamcard.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ImageFooter from "./components/ImageFooter/imgfooter";
 
@@ -13,7 +13,7 @@ function App() {
       <header className="App-header">
         <HeaderPart />
         <QuizLogic />
-        {/* <TeamCard /> */}
+        <TeamCard />
         <ImageFooter />
         <Footer />
       </header>
