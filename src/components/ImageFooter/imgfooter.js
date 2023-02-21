7a87eaffac810a3f849a05b94905316f8d7b7304
Husain Hidayah <husain.fikri@gmail.com>
2023-02-21 20:57:07 +0000

    replace form in simple html to useState and hook

--- src/components/ImageFooter/imgfooter.js
+++ src/components/ImageFooter/imgfooter.js
@@ -1,7 +1,86 @@
-import React from "react";
+// import React from "react";
+// import "./style.css";
+
+// const ImageFooter = () => {
+//   return (
+//     <div className="imgfooter bg-image text-light d-flex align-items-center">
+//       <div className="row">
+//         <div className="subcopy col-md-6">
+//           <h5 className="">Newsletter Subscription</h5>
+//           <p className="">
+//             Subscribe to our educational quiz website and stay up-to-date with
+//             the latest geography quizzes and interactive content. Test your
+//             knowledge and learn about the world around us! We promise not to
+//             spam you or share your information with any third parties.
+//           </p>
+//         </div>
+//         <div className="newsform col-md-6 d-flex justify-content-end">
+//           <div className="subform ">
+//             <h5>Subscribe to our Newsletter:</h5>
+//             <form
+//               name="contact"
+//               method="POST"
+//               data-netlify="true"
+//               onSubmit="submit"
+//             >
+//               <input type="hidden" name="form-name" value="contact" />
+//               <div className="form-group">
+//                 <input
+//                   name="name"
+//                   type="text"
+//                   className="form-control form-control-sm rounded-6 transparentinput"
+//                   id="InputName"
+//                   placeholder="Name"
+//                 />
+//               </div>
+//               <div className="form-group">
+//                 <input
+//                   name="email"
+//                   type="email"
+//                   className="form-control form-control-sm rounded-6 transparentinput"
+//                   id="InputEmail"
+//                   placeholder="Email"
+//                 />
+//               </div>
+//               <button
+//                 type="submit"
+//                 className="btnsbmt btn btn-primary btn-block btn-sm rounded-6"
+//               >
+//                 Subscribe
+//               </button>
+//             </form>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ImageFooter;
+
+import React, { useState } from "react";
 import "./style.css";
 
 const ImageFooter = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(new FormData(form)).toString(),
+    })
+      .then(() => {
+        alert("Thank you for subscribing!");
+        setName("");
+        setEmail("");
+      })
+      .catch((error) => alert(error));
+  };
+
   return (
     <div className="imgfooter bg-image text-light d-flex align-items-center">
       <div className="row">
@@ -17,12 +96,7 @@ const ImageFooter = () => {
         <div className="newsform col-md-6 d-flex justify-content-end">
           <div className="subform ">
             <h5>Subscribe to our Newsletter:</h5>
-            <form
-              name="contact"
-              method="POST"
-              data-netlify="true"
-              onSubmit="submit"
-            >
+            <form name="contact" onSubmit={handleSubmit}>
               <input type="hidden" name="form-name" value="contact" />
               <div className="form-group">
                 <input
@@ -31,6 +105,8 @@ const ImageFooter = () => {
                   className="form-control form-control-sm rounded-6 transparentinput"
                   id="InputName"
                   placeholder="Name"
+                  value={name}
+                  onChange={(event) => setName(event.target.value)}
                 />
               </div>
               <div className="form-group">
@@ -40,6 +116,8 @@ const ImageFooter = () => {
                   className="form-control form-control-sm rounded-6 transparentinput"
                   id="InputEmail"
                   placeholder="Email"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
                 />
               </div>
               <button
@@ -57,4 +135,3 @@ const ImageFooter = () => {
 };
 
 export default ImageFooter;
-// eslint-disable-next-line
