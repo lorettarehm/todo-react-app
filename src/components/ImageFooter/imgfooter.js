6aa32733a077ca8f4b2c26cf86063f406617159d
Husain Hidayah <husain.fikri@gmail.com>
2023-02-22 18:28:30 +0000

    using react library reacat-netlify-form and no styling

--- src/components/ImageFooter/imgfooter.js
+++ src/components/ImageFooter/imgfooter.js
@@ -1,93 +1,123 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
+import { NetlifyForm, Honeypot } from "react-netlify-forms";
 import "./style.css";
 
-const validationSchema = Yup.object().shape({
-  name: Yup.string().required("Name is required"),
-  email: Yup.string().email("Invalid email").required("Email is required"),
-});
+// const validationSchema = Yup.object().shape({
+//   name: Yup.string().required("Name is required"),
+//   email: Yup.string().email("Invalid email").required("Email is required"),
+// });
 
-const ImageFooter = () => {
-  const handleFormSubmit = (values, { setSubmitting }) => {
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(values).toString(),
-    })
-      .then(() => {
-        alert("Thank you for subscribing!");
-        setSubmitting(false);
-      })
-      .catch((error) => alert(error));
-  };
+const ImageFooter = () => (
+  <NetlifyForm name="Contact" action="/thanks" honeypotName="bot-field">
+    {({ handleChange, success, error }) => (
+      <>
+        <Honeypot />
+        {success && <p>Thanks for contacting us!</p>}
+        {error && (
+          <p>Sorry, we could not reach our servers. Please try again later.</p>
+        )}
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" id="name" onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            type="text"
+            name="message"
+            id="message"
+            rows="4"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </>
+    )}
+  </NetlifyForm>
+);
 
-  return (
-    <div className="imgfooter bg-image text-light d-flex align-items-center">
-      <div className="row">
-        <div className="subcopy col-md-6">
-          <h5 className="">Newsletter Subscription</h5>
-          <p className="">
-            Subscribe to our educational quiz website and stay up-to-date with
-            the latest geography quizzes and interactive content. Test your
-            knowledge and learn about the world around us! We promise not to
-            spam you or share your information with any third parties.
-          </p>
-        </div>
-        <div className="newsform col-md-6 d-flex justify-content-end">
-          <div className="subform ">
-            <h5>Subscribe to our Newsletter:</h5>
-            <Formik
-              initialValues={{ name: "", email: "" }}
-              validationSchema={validationSchema}
-              onSubmit={handleFormSubmit}
-            >
-              {({ isSubmitting }) => (
-                <Form name="contact">
-                  <input type="hidden" name="form-name" value="contact" />
-                  <div className="form-group">
-                    <Field
-                      name="name"
-                      type="text"
-                      className="form-control form-control-sm rounded-6 transparentinput"
-                      id="InputName"
-                      placeholder="Name"
-                    />
-                    <ErrorMessage
-                      name="name"
-                      component="div"
-                      className="error-message"
-                    />
-                  </div>
-                  <div className="form-group">
-                    <Field
-                      name="email"
-                      type="email"
-                      className="form-control form-control-sm rounded-6 transparentinput"
-                      id="InputEmail"
-                      placeholder="Email"
-                    />
-                    <ErrorMessage
-                      name="email"
-                      component="div"
-                      className="error-message"
-                    />
-                  </div>
-                  <button
-                    type="submit"
-                    className="btnsbmt btn btn-primary btn-block btn-sm rounded-6"
-                    disabled={isSubmitting}
-                  >
-                    {isSubmitting ? "Submitting..." : "Subscribe"}
-                  </button>
-                </Form>
-              )}
-            </Formik>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+// {
+//   const handleFormSubmit = (values, { setSubmitting }) => {
+//     fetch("/", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//       body: new URLSearchParams(values).toString(),
+//     })
+//       .then(() => {
+//         alert("Thank you for subscribing!");
+//         setSubmitting(false);
+//       })
+//       .catch((error) => alert(error));
+//   };
+
+//   return (
+//     <div className="imgfooter bg-image text-light d-flex align-items-center">
+//       <div className="row">
+//         <div className="subcopy col-md-6">
+//           <h5 className="">Newsletter Subscription</h5>
+//           <p className="">
+//             Subscribe to our educational quiz website and stay up-to-date with
+//             the latest geography quizzes and interactive content. Test your
+//             knowledge and learn about the world around us! We promise not to
+//             spam you or share your information with any third parties.
+//           </p>
+//         </div>
+//         <div className="newsform col-md-6 d-flex justify-content-end">
+//           <div className="subform ">
+//             <h5>Subscribe to our Newsletter:</h5>
+//             <Formik
+//               initialValues={{ name: "", email: "" }}
+//               validationSchema={validationSchema}
+//               onSubmit={handleFormSubmit}
+//             >
+//               {({ isSubmitting }) => (
+//                 <Form name="contact">
+//                   <input type="hidden" name="form-name" value="contact" />
+//                   <div className="form-group">
+//                     <Field
+//                       name="name"
+//                       type="text"
+//                       className="form-control form-control-sm rounded-6 transparentinput"
+//                       id="InputName"
+//                       placeholder="Name"
+//                     />
+//                     <ErrorMessage
+//                       name="name"
+//                       component="div"
+//                       className="error-message"
+//                     />
+//                   </div>
+//                   <div className="form-group">
+//                     <Field
+//                       name="email"
+//                       type="email"
+//                       className="form-control form-control-sm rounded-6 transparentinput"
+//                       id="InputEmail"
+//                       placeholder="Email"
+//                     />
+//                     <ErrorMessage
+//                       name="email"
+//                       component="div"
+//                       className="error-message"
+//                     />
+//                   </div>
+//                   <button
+//                     type="submit"
+//                     className="btnsbmt btn btn-primary btn-block btn-sm rounded-6"
+//                     disabled={isSubmitting}
+//                   >
+//                     {isSubmitting ? "Submitting..." : "Subscribe"}
+//                   </button>
+//                 </Form>
+//               )}
+//             </Formik>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
 export default ImageFooter;
